639a8d5a4d4b7f85f942dda7df7f571d158a36b7
Marc-Andre Bourassa <boumar04@gmail.com>
2019-08-26 14:09:16 -0400

    npm -> yarn

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 dotfilesdir=~/wsl-dotfiles
 
 # list of files/folders to symlink in home
-dotfiles="bash_profile bashrc gitconfig profile eslintrc prettierrc"
+dotfiles="bashrc gitconfig profile eslintrc prettierrc"
 
 # create symlinks (will overwrite old dotfiles)
 for file in ${dotfiles}; do
@@ -17,8 +17,12 @@ done
 
 # update and install package lists
 echo "Updating and installing packages"
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y build-essential nodejs python
+sudo apt install -y build-essential nodejs python yarn
 sudo apt autoremove -y 
-sudo npm install -g eslint prettier eslint-config-prettier eslint-plugin-prettier eslint eslint-config-airbnb-base eslint-plugin-import
+sudo yarn global add eslint prettier eslint-config-prettier eslint-plugin-prettier eslint eslint-config-airbnb-base eslint-plugin-import
