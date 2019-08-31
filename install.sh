1c633eb8fd5c61bb2dae380296a836908ff04105
Marc-Andre Bourassa <boumar04@gmail.com>
2019-08-30 20:42:33 -0400

    add python

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ dotfiles="bashrc gitconfig profile eslintrc prettierrc"
 for file in ${dotfiles}; do
     echo "Creating symlink to $file in home directory."
     sudo ln -sf ${dotfilesdir}/.${file} ~/.${file}
-done
+done    
 
 # update and install package lists
 echo "Updating and installing packages"
@@ -22,6 +22,6 @@ sudo add-apt-repository universe
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y build-essential nodejs python fonts-firacode
+sudo apt install -y build-essential nodejs python3 python3-pip fonts-firacode
 sudo apt autoremove -y 
 sudo npm install -g eslint prettier eslint-plugin-prettier eslint-config-prettier
